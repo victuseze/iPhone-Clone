7611ad2739b0592290d0163a26289e2cd965b35b
victuseze <106381679+victuseze@users.noreply.github.com>
2023-02-07 18:05:33 +0000

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+let displayed = document.querySelector('.display')
+let menu = document.querySelector('#menu')
+let shop = document.querySelector('.shop')
+let services = document.querySelector('.services')
+let appleStore = document.querySelector('.apple-store')
+let business = document.querySelector('.business')
+let values = document.querySelector('.values')
+let apples = document.querySelector('.apple1')
+
+menu.addEventListener('click', function(){
+    displayed.style.display = 'block';
+})
